5771a34b5647be145a8c6fee73c520d6f8b5563d
Sazidul Alam <sazidulalam20@gmail.com>
2022-03-28 18:15:45 +0600

    learnig DOM 1

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-//module 18 lecture 
+//module 18 lecture 5
+
+let heading = document.getElementsByTagName('h1');
+
+for (let i = 0; i < heading.length; i++) {
+    let element = heading[i];
+    element.style.color = '#4545ff';
+}
+
+function cngTxt() {
+    let para = document.getElementById('para');
+    para.innerText = 'You have been changed!!!';
+    para.style.color = 'red';
+    para.style.fontSize = '35px';
+}
+
+function cng() {
+    let box = document.getElementsByClassName('box');
+    for (let i = 0; i < box.length; i++) {
+        const element = box[i];
+        element.style.backgroundColor = 'yellow';
+    }
+}
+
+let specialHeading = document.querySelector('.box-special h1');
+specialHeading.style.color = 'green';
+
+let specialPara = document.querySelectorAll('.box-special p');
+for (let i = 0; i < specialPara.length; i++) {
+    const element = specialPara[i];
+    element.style.backgroundColor = 'lightblue';
+}
