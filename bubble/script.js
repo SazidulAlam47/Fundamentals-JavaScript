331be2e0c8ae848f1d0984fbab2d5be3ad05b98d
Sazidul Alam <sazidulalam20@gmail.com>
2022-04-10 20:06:27 +0600

    event bubble finished

--- bubble/script.js
+++ bubble/script.js
@@ -1,3 +1,4 @@
+/*
 document.getElementById('first').addEventListener('click', function(){
     console.log('first clicked');
     event.stopPropagation();
@@ -7,4 +8,25 @@ document.getElementById('list').addEventListener('click', function(){
 })
 document.getElementById('container').addEventListener('click', function(){
     console.log('container clicked');
+})
+*/
+/*
+let allItems = document.getElementsByClassName('item');
+for (let i = 0; i < allItems.length; i++) {
+    let item = allItems[i];
+    item.addEventListener('click', function(event){
+        event.target.parentNode.removeChild(event.target);
+    })
+}
+*/
+document.getElementById('new-item').addEventListener('click', function(){
+    let newItem = document.createElement('li');
+    newItem.innerHTML = 'simply dummy text of the printing and typesetting industry';
+    newItem.setAttribute('class', 'item'); 
+    document.getElementById('list').appendChild(newItem);
+})
+
+document.getElementById('list').addEventListener('click', function(event){
+    console.log(this, event.target);
+    this.removeChild(event.target)
 })
