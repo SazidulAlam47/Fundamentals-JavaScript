c6c3a8f7e4b4ef6000d1129ed1669338ee4a5d36
Sazidul Alam <sazidulalam20@gmail.com>
2023-01-28 00:16:58 +0600

    14 module done

--- 14 APPLY JAVASCRIPT CONCEPTS/11.fiboRecursiveNumber.js
+++ 14 APPLY JAVASCRIPT CONCEPTS/11.fiboRecursiveNumber.js
@@ -1,9 +1,11 @@
 function fibonacci(n) {
     if (n == 0) {
         return 0;
-    } else if (n == 1) {
+    } 
+    else if (n == 1) {
         return 1;
-    } else {
+    } 
+    else {
         return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
--- 14 APPLY JAVASCRIPT CONCEPTS/12.fiboRecursiveArray.js
+++ 14 APPLY JAVASCRIPT CONCEPTS/12.fiboRecursiveArray.js
@@ -1,9 +1,12 @@
 function fibonacci(n) {
     if (n == 0) {
         return [0];
-    } else if (n == 1) {
+    }
+    else if (n == 1) {
         return [0, 1];
-    } else {
+    } 
+    else {
+        // calculate array nth element
         let fibo = fibonacci(n - 1);
         let nextElement = fibo[n - 1] + fibo[n - 2];
         fibo.push(nextElement);
