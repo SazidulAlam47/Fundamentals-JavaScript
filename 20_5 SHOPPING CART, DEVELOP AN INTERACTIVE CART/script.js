0a78dbbf3e491285d813643d53e9e53f6681b2b1
Sazidul Alam <momin@ABDULs-MacBook-Pro.local>
2023-02-28 23:14:24 +0600

    shoping cart done JS

--- 20_5 SHOPPING CART, DEVELOP AN INTERACTIVE CART/script.js
+++ 20_5 SHOPPING CART, DEVELOP AN INTERACTIVE CART/script.js
@@ -1,24 +1,34 @@
-let plusIcons = document.querySelectorAll('.cart-item .fas.fa-plus');
-for (let index = 0; index < plusIcons.length; index++) {
-    const plusIcon = plusIcons[index];
-    const plusBtn = plusIcon.parentNode;
-    plusBtn.addEventListener('click', function(){
-        const productCount = plusBtn.parentNode.childNodes[3];
-        let productCountNumber = parseFloat(productCount.value);
-        productCountNumber++;
-        productCount.value = productCountNumber;
-    })
-    console.log(plusBtn.parentNode.parentNode);
-}
+function plusMinusCount(btnId, countId, plusOrMinus, pirceId, unitPrice){
+    document.getElementById(btnId).addEventListener('click', function(){
+        // update count
+        const count = document.getElementById(countId);
+        let countNumber = parseInt(count.value);
+        if(plusOrMinus == 'plus') {
+            countNumber++;
+        }
+        else if(plusOrMinus == 'minus'){
+            if(countNumber > 0) countNumber--;
+        }
+        count.value = countNumber;
 
-let minusIcons = document.querySelectorAll('.cart-item .fas.fa-minus');
-for (let index = 0; index < minusIcons.length; index++) {
-    const minusIcon = minusIcons[index];
-    const minusBtn = minusIcon.parentNode;
-    minusBtn.addEventListener('click', function(){
-        const productCount = minusBtn.parentNode.childNodes[3];
-        let productCountNumber = parseFloat(productCount.value);
-        if (productCountNumber > 0) productCountNumber--;
-        productCount.value = productCountNumber;
+        //update the product price
+        let productPriceNumber = unitPrice * countNumber;
+        console.log(productPriceNumber);
+        document.getElementById(pirceId).innerText = productPriceNumber;
+
+        // update total price
+        const phonePrice = document.getElementById('phone-price');
+        const casePirice = document.getElementById('case-price');
+        const subtotalPrice = document.getElementById('subtotal-price');
+        subtotalPrice.innerText = parseInt(phonePrice.innerText) + parseInt(casePirice.innerText);
+        const totalPrice = document.getElementById('total-price');
+        totalPrice.innerText = parseInt(phonePrice.innerText) + parseInt(casePirice.innerText);
+        
+
     })
 }
+plusMinusCount('case-plus', 'case-count', 'plus', 'case-price', 59);
+plusMinusCount('case-minus', 'case-count', 'minus', 'case-price', 59);
+
+plusMinusCount('phone-plus', 'phone-count', 'plus', 'phone-price', 1219);
+plusMinusCount('phone-minus', 'phone-count', 'minus', 'phone-price', 1219);
