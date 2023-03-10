bf9c2764502b43078a3eb038215839bbf90d9cd3
Sazidul Alam <sazidulalam20@gmail.com>
2023-03-10 16:34:48 +0600

    code update like a pro

--- 20_5 SHOPPING CART, DEVELOP AN INTERACTIVE CART/script.js
+++ 20_5 SHOPPING CART, DEVELOP AN INTERACTIVE CART/script.js
@@ -1,20 +1,21 @@
-function plusMinusCount(btnId, countId, plusOrMinus, pirceId, unitPrice){
-    document.getElementById(btnId).addEventListener('click', function(){
+function plusMinusCount(product, isPlus, unitPrice){
+    const plusOrMinus = isPlus == true ? "plus" : "minus";
+    document.getElementById(product + '-' + plusOrMinus).addEventListener('click', function(){
         // update count
-        const count = document.getElementById(countId);
+        const count = document.getElementById(product + '-count');
         let countNumber = parseInt(count.value);
         if(plusOrMinus == 'plus') {
             countNumber++;
         }
-        else if(plusOrMinus == 'minus'){
-            if(countNumber > 0) countNumber--;
+        else if(countNumber > 0){
+            countNumber--;
         }
         count.value = countNumber;
 
         //update the product price
         let productPriceNumber = unitPrice * countNumber;
         console.log(productPriceNumber);
-        document.getElementById(pirceId).innerText = productPriceNumber;
+        document.getElementById(product + '-price').innerText = productPriceNumber;
 
         // update total price
         const phonePrice = document.getElementById('phone-price');
@@ -27,10 +28,9 @@ function plusMinusCount(btnId, countId, plusOrMinus, pirceId, unitPrice){
 
     })
 }
-plusMinusCount('case-plus', 'case-count', 'plus', 'case-price', 59);
-plusMinusCount('case-minus', 'case-count', 'minus', 'case-price', 59);
+plusMinusCount('phone', true, 1219);
+plusMinusCount('phone', false, 1219);
 
-plusMinusCount('phone-plus', 'phone-count', 'plus', 'phone-price', 1219);
-plusMinusCount('phone-minus', 'phone-count', 'minus', 'phone-price', 1219);
-
+plusMinusCount('case', true, 59);
+plusMinusCount('case', false, 59);
 //done
