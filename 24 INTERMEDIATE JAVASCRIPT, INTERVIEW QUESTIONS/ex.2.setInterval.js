e299f0b8ed1b05dadac0e88d0dd6cd5816ac6c9d
Sazidul Alam <sazidulalam20@gmail.com>
2023-08-19 23:07:38 +0600

    extra 24 done

---
+++ 24 INTERMEDIATE JAVASCRIPT, INTERVIEW QUESTIONS/ex.2.setInterval.js
@@ -0,0 +1,12 @@
+console.log("first");
+
+const secondPrint = () => console.log("second");
+
+setTimeout(secondPrint, 4000);
+
+setInterval(() => {
+    console.log("amare treat dey");
+}, 2000);
+console.log("third");
+console.log("fourth");
+console.log("fifth");
---
+++ 24 INTERMEDIATE JAVASCRIPT, INTERVIEW QUESTIONS/ex.3.date.js
@@ -0,0 +1,20 @@
+const time = new Date();
+console.log(time);
+
+let yr = time.getFullYear();
+console.log(yr);
+
+let mth = time.getMonth();
+console.log(mth);
+
+let dt = time.getDate();
+console.log(dt);
+
+let hr = time.getHours();
+console.log(hr);
+
+let mt = time.getMinutes();
+console.log(mt);
+
+let sc = time.getSeconds();
+console.log(sc);
