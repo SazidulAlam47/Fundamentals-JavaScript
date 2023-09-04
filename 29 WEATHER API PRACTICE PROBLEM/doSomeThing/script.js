1c6a9eeeedff5af00c97ecea3924d3d64d562c59
Sazidul Alam <sazidulalam20@gmail.com>
2023-09-04 19:25:00 +0600

    display number fixed

--- 29 WEATHER API PRACTICE PROBLEM/doSomeThing/script.js
+++ 29 WEATHER API PRACTICE PROBLEM/doSomeThing/script.js
@@ -9,15 +9,15 @@ loadData();
 function doSomeThing(data){
     console.log(data);
     const work = document.createElement('div');
-    work.setAttribute('class', 'card');
+    work.classList.add('card');
     work.innerHTML = `
         <h1>${data.activity}</h1>
         <p>${data.type}</p>
         <h3>Cost: $ ${data.price} </h3>
     `;
-    if(data.link !== ''){
+    if(data.link){
         const theLink = document.createElement('a');
-        theLink.innerHTML = 'Click here to learn more';
+        theLink.innerText = 'Click here to learn more';
         theLink.setAttribute('href', data.link);
         theLink.setAttribute('target', '_blank');
         work.appendChild(theLink);
